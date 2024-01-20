e342fc49282d3363bd10b346780015297b5e220a
Ruthran <140536167+Ruthran28@users.noreply.github.com>
2024-01-20 08:57:31 +0530

    Add files via upload

--- conU.js
+++ conU.js
@@ -1,6 +1,8 @@
-const modU= require('./model/usermod')
+const modU= require('../model/usermod')
 const bcrypt=require('bcrypt')
 const nodemailer=require('nodemailer')
+const sermodu=require('../model/sermod')
+const expmod=require('../model/expensemod')
 const register=async(req,res)=>{
 
     const sender=nodemailer.createTransport({
@@ -11,8 +13,9 @@ const register=async(req,res)=>{
         }
     })
 
-    const{name,aadhaar,email,phone, room, address,password,blockname}=req.body
+    const{name,aadhaar,email,phone,room, address,password,blockname}=req.body
     let exUser;
+    let user;
     try{
         exUser=await modU.findOne({email:email})
     }
@@ -24,15 +27,24 @@ const register=async(req,res)=>{
         return res.status(404).json({message:'exist'})
     }
 
-     const uniqueid=`Auroitech_${blockname}_${room} `
 
-    const composemail={
+    let exroom= await modU.findOne({room:room})
+
+    if(exroom){
+        return res.status(404).json({message:'room already booked'})
+    }
+
+     const uniqueid=`Auroitech${blockname}${room}`
+
+    
+     const composemail={
         from:'jo214841@gmail.com',
-        to:'lokesh0405062005@gmail.com',
+        to:'sjhruthran28@gmail.com',
         subject:'welcome to Auroitech',
         text:`this is you unique id please keep secert this code  ${uniqueid} `
     }
-       
+   
+
     await sender.sendMail(composemail,(error,info)=>{
   
         if(error){
@@ -42,10 +54,8 @@ const register=async(req,res)=>{
         console.log('send sucessfully')
         
         console.log(info.messageId)
-        return res.status(404).json({mydata})
        })
-
-     let hide=bcrypt.hashSync(password,10)
+     
     try {
         user=new modU({
         name,
@@ -54,8 +64,9 @@ const register=async(req,res)=>{
         phone,
         room, 
         address,
-        password:hide,
-        uniquecode:uniqueid
+        password,
+        uniquecode:uniqueid,
+        blockname
     })
         user=await user.save()
     } 
@@ -63,45 +74,104 @@ const register=async(req,res)=>{
      {
             console.log(error)
     }
-   
-
-    return res.status(200).json({user})
+  
+    return res.status(200).json("success")
 
 }
-const signin=async(req,res)=>{
+const login=async(req,res)=>{
 
-    const {email,password,uniquecode}=req.body
- 
-    let existuser;
- 
-    try {
-       existuser=await con.findOne({email:email});
-    } catch (error) {
-       console.log(error)
-    }
-    if(!existuser){
-       return res.status(404).json({message:"user id not found"})
- 
-    }
-    const code=await modU.find({uniquecode:uniquecode})
+ const{uniquecode}=req.body
 
-    if(!code){
-        return res.status(404).json({message:"uniquecode is incorroct"})
-    }
-    const ispasswordcrt=bcrypt.compareSync (password,existuser.password)
+ let user;
+
+ try {
+    user=await modU.find({uniquecode:uniquecode})
+
+ } catch (error) {
+  
+    console.log(error)
     
-    if(!ispasswordcrt){
-       return res.status(404).json({message:"passsword is incorroct"})
-    }
-    const token=jwt.sign({id:existuser._id},JWT_KEY,{
-       expiresIn:"1hr"
-    })
-    return res.status(200).json({message:"login success fully",user:existuser,token})
+ }
+
+ if(!user){
+    return res.json("unique id invalid")
+ }
+
+ return res.status(200).json("success")
+
+
+
+
+}
+
+  const service=async(req,res)=>{
+
+   
+    const{tanantname,roomnum,typeofcomplaint,complaintdeatails}=req.body
+
+
+     let user;
+
+    let crctroom=await modU.findOne({room:roomnum})
  
+     if(!crctroom){
+ 
+        return res.status(404).json({message:"room is incorrect"})
+     }
+
+     let curenntuser;
+
+     try {
+        
+        curenntuser=new sermodu({
+
+            tanantname,roomnum,typeofcomplaint,complaintdeatails,repairstatus:true
+        })
+
+        curenntuser=await curenntuser.save();
+     } catch (error) {
+        console.log(error)
+     }
+
+
+
+     return res.status(200).json(curenntuser)
+
+
   }
 
+  const resolvethereparis=async()=>{
 
+    const sender=nodemailer.createTransport({
+        service:'gmail',
+        auth:{
+            user:'jo214841@gmail.com',
+            pass:'xzwb jwbd qosi lzaz'
+        } 
+    })
+    const composemail={
+        from:'jo214841@gmail.com',
+        to:'lokesh0405062005@gmail.com',
+        subject:'welcome to Auroitech',
+        text:`this is you unique id please keep secert this code `
+    }
+    await sender.sendMail(composemail,(error,info)=>{
+  
+        if(error){
+            console.log(error.message)
+            return;
+        }
+        console.log('send sucessfully')
+        
+        console.log(info.messageId)
+       })
+      
+       await expmod.updateOne({repairstatus:false})
+
+  }
+  
 
 
 exports.register=register
-exports.signin=signin
+exports.login=login
+exports.service=service
